d39f0f9578c87bf692fa4cf68cf4d091cc742501
KHAREL BILOA <kharelbiloa@gmail.com>
2026-03-23 19:39:13 +0100

    Fix formatting of CORS middleware configuration

--- main.py
+++ main.py
@@ -27,12 +27,11 @@ async def read_index():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["*"],  
     allow_credentials=True,
-    allow_methods=["*"], 
-    allow_headers=["*"],
+    allow_methods=["*"],  
+    allow_headers=["*"],  
 )
-
 
 
 def get_db():
